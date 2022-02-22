ccc79ca9d5396004cf3c0afd5f3bcdef253fc4b1
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-22 13:14:58 +0530

    Mark as Complete

--- src/functions.php
+++ src/functions.php
@@ -1,8 +1,6 @@
 <?php 
-    session_start();
-
     function addList($list){
-        $lists = isset($_SESSION['lists'])?$_SESSION['lists']:array();
+        $lists = (isset($_SESSION['lists']))?$_SESSION['lists']:array();
         array_push($lists, $list);
         $_SESSION['lists'] = $lists;
     }
@@ -12,11 +10,26 @@
             echo '
             <form action="" method = "POST">
                 <li>    
-                    <input type="checkbox">
+                    <input type="checkbox" name = "action" onchange="this.form.submit()" value = "checkBox">
                     <label>'.$value.'</label><input type="text">
                     <button class="edit" name="action" value="edit">Edit</button>
                     <button class="delete" name = "action" value = "delete">Delete</button>
                     <input type = "text" hidden name = "hid_val" value = "'.$key.'"> 
+                </li>
+            </form>';
+        } 
+    }
+
+    function displayCompletedList($valChecked){
+        foreach($_SESSION['completed'] as $key=>$value){
+            echo '
+            <form action="" method = "POST">
+                <li>    
+                    <input type="checkbox" name = "action" onchange="this.form.submit()" value = "unchecked" '.$valChecked.'>
+                    <label>'.$value.'</label><input type="text">
+                    <button class="edit" name="action" value="edit1">Edit</button>
+                    <button class="delete" name = "action" value = "delete1">Delete</button>
+                    <input type = "text" hidden name = "hid_val1" value = "'.$key.'"> 
                 </li>
             </form>';
         } 
--- src/todo.php
+++ src/todo.php
@@ -2,6 +2,8 @@
     session_start();
     include 'functions.php';
     $action = '';
+    $flag = 0;
+    
     if (isset($_POST['action'])){
         $list = $_POST['list'];
         $action = $_POST['action'];
@@ -14,17 +16,49 @@
                 $del_id = $_POST['hid_val'];
                 array_splice($_SESSION['lists'], $list_id, 1);
                 break;
+            case 'delete1':
+                $del_id = $_POST['hid_val1'];
+                array_splice($_SESSION['completed'], $del_id, 1);
+                break;
             case 'edit':
+                $flag = 1;
                 $edit_field = $_POST['hid_val'];
                 $_SESSION['id'] = $edit_field;
+                break;
+        
+            case 'edit1':
+                $edit_field1 = $_POST['hid_val1'];
+                $_SESSION['id1'] = $edit_field1;
                 break;
             case 'update':
                 $updatedList = $_POST['list'];
                 $_SESSION['lists'][$_SESSION['id']] = $updatedList;
+                $flag = 0;
                 break;
+            case 'checkBox':
+                $valChecked = 'checked';
+                $check_id = $_POST['hid_val'];
+                $listChecked = $_SESSION['lists'][$check_id];
+
+                $completed = isset($_SESSION['completed'])?$_SESSION['completed']:array();
+                array_push($completed, $listChecked);
+                $_SESSION['completed'] = $completed;
+
+                array_splice($_SESSION['lists'], $check_id, 1);
+                break;
+            case 'unchecked':
+                $uncheck_id = $_POST['hid_val1'];
+                $listUnChecked = $_SESSION['completed'][$uncheck_id];
+                
+                $lists = isset($_SESSION['lists'])?$_SESSION['lists']:array();
+                array_push($lists, $listUnChecked);
+                $_SESSION['lists'] = $lists;
+
+                array_splice($_SESSION['completed'], $uncheck_id, 1);
+                break;
+
         }
     }
-
 ?>
 
 <html>
@@ -40,8 +74,11 @@
             <form action="" method="POST">
             <p>
                 <input id="new-task" name="list" type="text" value="<?php echo $_SESSION['lists'][$edit_field]; ?>">
-                <input type = 'submit' name="action" value="add">
-                <input type = 'submit' name="action" value="update">
+                <?php if ($flag == 1): ?>
+                    <input type = 'submit' name="action" value="update">
+                <?php else: ?>
+                    <input type = 'submit' name="action" value="add">
+                <?php endif; ?>
             </p>
             <h3>Todo</h3>
             <ul id="incomplete-tasks">
@@ -51,8 +88,7 @@
     
             <h3>Completed</h3>
             <ul id="completed-tasks">
-                <li><input type="checkbox" checked>
-                <label>See the Doctor</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>
+            <?php displayCompletedList($valChecked); ?>
             </ul>
             </form>
 
